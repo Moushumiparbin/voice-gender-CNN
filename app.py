bc7f77d797d3746ea35e01fb59a66f1b2b2ca894
Moushumiparbin <moushumiparbin8@gmail.com>
2026-05-22 20:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,68 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
-import tempfile
-from utils import predict_audio
+import os
+from pydub import AudioSegment
+from tensorflow.keras.models import load_model
+from utils import extract_features
+from collections import Counter
 
-st.set_page_config(page_title="Speech Gender Classification", layout="centered")
+# Load model
+model = load_model("cnn_gender_model.keras")
 
-st.title("🎤 Assamese Speech Gender Classification")
-st.write("Upload a WAV file to predict gender")
+st.set_page_config(page_title="Voice Gender Detection", layout="centered")
 
-import keras
-import streamlit as st
+st.title("🎤 Voice Gender Classification (CNN)")
+st.write("Upload a WAV file and predict gender")
 
-@st.cache_resource
-def load_model():
-    model = keras.models.load_model(
-        "hybrid_model.keras",
-        compile=False,
-        custom_objects={}
-    )
-    return model
+uploaded_file = st.file_uploader("Upload Audio", type=["wav"])
 
-model = load_model()
+def predict(file_path, threshold=0.5):
 
-uploaded_file = st.file_uploader("Upload Audio File", type=["wav"])
+    audio = AudioSegment.from_wav(file_path)
+    predictions = []
+
+    for i in range(0, len(audio), 3000):
+
+        chunk = audio[i:i+3000]
+
+        if chunk.dBFS == float("-inf") or chunk.dBFS < -55:
+            continue
+
+        temp_file = "temp.wav"
+        chunk.export(temp_file, format="wav")
+
+        feat = extract_features(temp_file)
+
+        feat = feat[np.newaxis, ..., np.newaxis]
+
+        prob = model.predict(feat, verbose=0)[0][0]
+
+        label = "MALE" if prob > threshold else "FEMALE"
+        predictions.append(label)
+
+    if len(predictions) == 0:
+        return None, 0
+
+    final_label = Counter(predictions).most_common(1)[0][0]
+    confidence = Counter(predictions)[final_label] / len(predictions)
+
+    return final_label, confidence
+
 
 if uploaded_file is not None:
 
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
-        tmp.write(uploaded_file.read())
-        file_path = tmp.name
+    file_path = os.path.join("temp.wav")
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.read())
 
     st.audio(uploaded_file)
 
-    st.info("Processing...")
+    if st.button("Predict Gender"):
 
-    label, confidence = predict_audio(file_path, model)
+        label, conf = predict(file_path)
 
-    st.success(f"🎯 Prediction: {label}")
-    st.write(f"📊 Confidence: {confidence*100:.2f}%")
+        if label is None:
+            st.warning("No speech detected")
+        else:
+            st.success(f"🎯 Prediction: {label}")
+            st.info(f"📊 Confidence: {conf:.3f}")
